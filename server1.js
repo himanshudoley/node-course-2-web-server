4da269bc1cf2f0f3325e2a427ec2689c32119884
Himanshu <himanshu@Himanshus-MacBook-Air.local>
2018-11-10 17:24:46 +0530

    Initial commit

---
+++ server1.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+
+var app=express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs'); //handlebars
+
+
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{
+	var now=new Date().toString();
+	var log='${now}: ${req.method} ${req.url}';
+	console.log(log);
+	fs.appendFile('server.log',log+ '\n',(err)=>{
+		if(err){
+			console.log('unable to append to server.log.')
+		}
+	});
+	next();
+});
+
+hbs.registerHelper('getCurrentyear',()=>{
+	return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt',(text)=>{
+	return text.toUpperCase();
+})
+
+app.get('/',(req,res)=>{
+	res.render('home.hbs',{
+		pagetitle: 'home page',
+		welcomemessage: 'welcome to my message',
+		//currentyear: new Date().getFullYear()
+	});		//handlebars
+});
+
+app.get('/about',(req,res)=>{
+	res.render('about.hbs',{
+		pagetitle: 'About page',
+		//currentyear: new Date().getFullYear()
+	});		//handlebars
+});
+
+app.listen(3000,()=>{
+	console.log('server is up on');
+});
